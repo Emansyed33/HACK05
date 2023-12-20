f0a35117d02479d33c30a7bf4f4ff6056f0a4787
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-12-21 00:00:18 +0500

    writing prototypes for the calling of functions to convert degrees to radian plus distance and time dilation

---
+++ utilis.h.c
@@ -0,0 +1,7 @@
+//prototypes: 
+//functions to convert degrees to radians
+double degreesToRadians(double degree);
+//function to compute the air distance between two locations
+double getAirdistance(double originLatitude, double originLongitude, double destinationLatitude, double destinationLongitude);
+//functions for time dilation using lorentz
+double lorentzTimedilation(double t, double percentC);
